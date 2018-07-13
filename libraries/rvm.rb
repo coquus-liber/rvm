1b0d7177fbc7402e93467a93182236335f7daad1
Robert Ferney <rob@ferney.org>
2018-07-12 23:21:49 -0600

    saveing end of day thursday

--- libraries/rvm.rb
+++ libraries/rvm.rb
@@ -1,45 +1,125 @@
 
+require 'pathname'
+
 module RVM
+  def rvm_node
+    @rvm_node ||= node['rvm'] || {}
+  end
+
+  def rvm_user 
+    @rvm_user ||= rvm_node['user'] || 'vagrant'
+  end
+
+  def rvm_group
+    @rvm_group ||= rvm_node['group'] || rvm_user
+  end
+
+  def home
+    @home ||= Pathname.new(::Dir.home(rvm_user))
+  end
+
+  def rvm_dir
+    @rvm_dir ||= home.join('.rvm')
+  end
+
+  def src_dir
+    @src_dir ||= rvm_dir.join('src')
+  end
+
+  def bin_dir
+    @bin_dir ||= rvm_dir.join('bin')
+  end
+
+  def rvm_bin
+    @rvm_bin ||= bin_dir.join('rvm')
+  end
+
+  def installed_file
+    @installed_file ||= rvm_dir.join("installed.at")
+  end
+
+  def rvm_installed? 
+    installed_file.exist? 
+  end
+
+  def key_file
+    @key_file ||= rvm_dir.join("mpapis.asc")
+  end
+
+  def rvm_repo
+    @rvm_repo ||= 'https://github.com/rvm/rvm.git'
+  end
+
+  def revision 
+    @revision ||= 'stable'
+  end
+
+  def key_source
+    @key_source ||= 'https://rvm.io/mpapis.asc'
+  end
+
   def install_rvm
-    directory rvm_dir do
-      user new_resource.user
-      group new_resource.group
+    directory rvm_dir.to_s do
+      user self.rvm_user
+      group self.rvm_group
       mode '0755'
     end
 
-    remote_file key_file do
-      source new_resource.key_source
-      owner new_resource.user
-      group new_resource.group
+    remote_file key_file.to_s do
+      source self.key_source
+      owner self.rvm_user
+      group self.rvm_group
       mode '0644'
       action :create
     end
 
-    rvm_key user do
-      key_file new_resource.key_file
+    rvm_key rvm_user do
+      key_file self.key_file.to_s
       action :import
     end
 
-    git src_dir do
-      repository repo
-      revision new_resource.revision
-      user new_resource.user
-      group new_resource.group
+    git src_dir.to_s do
+      repository self.rvm_repo
+      revision self.revision
+      user self.rvm_user
+      group self.rvm_group
     end
 
     execute 'install rvm' do
-      cwd src_dir
-      user new_resource.user
-      group new_resource.user
+      cwd src_dir.to_s
+      user self.rvm_user
+      group self.rvm_user
       environment(
-        'USER': new_resource.user,
-        'USERNAME': new_resource.user,
-        'LOGNAME': new_resource.user
+        'USER': self.rvm_user,
+        'USERNAME': self.rvm_user,
+        'LOGNAME': self.rvm_user
       )
       command "./install"
-      creates installed
+      creates installed_file.to_s
       live_stream true
     end
   end
 
+  def rvm_exec *cmd
+    execute "rvm #{cmd.join(' ')}" do
+      cwd self.home.to_s
+      user self.rvm_user
+      group self.rvm_user
+      environment(
+        'USER': self.rvm_user,
+        'USERNAME': self.rvm_user,
+        'LOGNAME': self.rvm_user
+      )
+      command([rvm_bin.to_s, *cmd].join(' '))
+      live_stream true
+    end
+  end
+
+  def rvm_install *rubies
+    rvm_exec 'install', *rubies
+  end
+
+  def rvm_use ruby
+    rvm_exec 'use', ruby
+  end
 end
--- resources/default.rb
+++ resources/default.rb
@@ -1,54 +1,22 @@
 
 property :command, [String, Array], name_property: true
-property :user, String
-property :group, String
-property :repo, String, default: 'https://github.com/rvm/rvm.git'
-property :revision, String, default: 'stable'
-property :home_dir, String
-property :rvm_dir, String
-property :installed, String
-property :key_source, String, default: 'https://rvm.io/mpapis.asc'
-property :key_file, String
-property :src_dir, String
-property :ruby, [String, Array]
 property :version, [String, Array]
 
 # default[:rvm][:rubies] = %w(ruby-2.4.4 ruby-2.5.1)
 # default[:rvm][:use] = "ruby-2.4.4"
 
-action_class do 
-  def rvm_installed? 
-    ::File.exists? installed
-  end
-
-  include InstallRVM
+load_current_value do
 end
 
-load_current_value do
-  rvm = node['rvm']
-  user(rvm['user'] || vagrant) unless property_is_set? :user
-  group(rvm['group'] || user) unless property_is_set? :group
-  home_dir(rvm['home_dir'] || "/home/#{user}") unless property_is_set? :home_dir
-  rvm_dir(rvm['rvm_dir'] || home_dir + '/.rvm' ) unless property_is_set? :rvm_dir
-  src_dir(rvm['src_dir'] || rvm_dir + '/src' ) unless property_is_set? :src_dir
-  installed(rvm['installed'] || "#{rvm_dir}/installed.at" ) unless property_is_set? :installed
-  key_file(rvm['key_file'] || "#{rvm_dir}/mpapis.asc" ) unless property_is_set? :key_file
-  install_rvm unless rvm_installed?
+action_class do 
+  include RVM
 end
 
 action :run do
-  execute "rvm #{new_resource.command}" do
-    cwd new_resource.home_dir
-    user new_resource.user
-    group new_resource.user
-    environment(
-      'USER': new_resource.user,
-      'USERNAME': new_resource.user,
-      'LOGNAME': new_resource.user
-    )
-    command "~/.rvm/bin/rvm #{new_resource.command}"
-    live_stream true
-  end
+  install_rvm unless rvm_installed?
+  rvm_install 'ruby'
+  rvm_use 'ruby'
+  rvm_exec new_resource.command
 end
 
 
