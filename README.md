0a2aed52f1dfdefdd8933c499b5f71927dc04656
Robert Ferney <rob@ferney.org>
2018-09-19 11:21:01 -0600

    docker kitchen file

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 TODO: Enter the cookbook description here.
 
+chef gem install kitchen-docker
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,22 @@
+
+package 'git'
+
+user 'vagrant' do
+end
+
+group 'vagrant' do
+  members 'vagrant'
+end
+
+group 'sudoers' do
+  members 'vagrant'
+end
+
+file "/etc/sudoers.d/99_vagrant" do
+  content "vagrant ALL=(ALL) NOPASSWD:ALL"
+end
+  
+
 rvm "vagrant" do
   ruby 'ruby'
   gems %w(rails)
