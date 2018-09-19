c9fa18b4be2e8f51845cb8ba6ec4b4618765e08a
Robert Ferney <capnregex@gmail.com>
2018-09-19 15:44:10 -0600

    update kitchen

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,11 @@
 
 package 'git'
+package 'gnupg2'
 
 user 'vagrant' do
+  home '/home/vagrant'
+  shell '/bin/bash'
+  manage_home true
 end
 
 group 'vagrant' do
@@ -14,6 +18,11 @@ end
 
 file "/etc/sudoers.d/99_vagrant" do
   content "vagrant ALL=(ALL) NOPASSWD:ALL"
+end
+
+directory "/home/vagrant" do
+  owner "vagrant"
+  group "vagrant"
 end
   
 
